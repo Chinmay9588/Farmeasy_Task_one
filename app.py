9720f8ae4445f9c1f3579a3657e8bb1e97963eb7
Chinmay Deshmukh <deshmukhchinmay01@gmail.com>
2025-11-20 14:35:34 +0530

    Recommit all project updates including Render deployment files

--- app.py
+++ app.py
@@ -135,4 +135,4 @@ def get_insights():
 # RUN FLASK APP
 # -----------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=10000)
